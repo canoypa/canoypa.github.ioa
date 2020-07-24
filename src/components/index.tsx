a2186fbe8f00e3b3c3ca7f958755c32a876b3e44
Cano <contact.sasasaky@gmail.com>
2020-07-25 00:16:51 +0900

    preact FC type def
    FunctionComponent が未使用判定になるけれど

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,8 @@
-import { h, FunctionComponent } from "preact";
+import { h, FC } from "preact";
 import { SocialButton } from "./SocialButton/SocialButton";
 import styles from "./styles.scss";
 
-export const App: FunctionComponent = () => (
+export const App: FC = () => (
   <main class={styles.main}>
     <SocialButton type="GitHub" />
     <SocialButton type="NPM" />
