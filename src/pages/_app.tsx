d83d7151b03901810d239736a1807197aff742f6
Cano <contact.sasasaky@gmail.com>
2022-08-12 19:14:54 +0900

    no use emotion cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,11 @@
-import { CacheProvider } from "@emotion/react";
-import { EmotionCache } from "@emotion/utils";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../components/providers/ThemeProvider";
-import createEmotionCache from "../core/emotion-cache";
 
-type Props = AppProps & {
-  emotionCache?: EmotionCache;
-};
-function MyApp({
-  Component,
-  pageProps,
-  emotionCache = createEmotionCache(),
-}: Props) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,4 @@
-import createEmotionServer from "@emotion/server/create-instance";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { Children, FC } from "react";
-import createEmotionCache from "../core/emotion-cache";
 
 class MyDocument extends Document {
   render() {
@@ -17,35 +14,3 @@ class MyDocument extends Document {
   }
 }
 export default MyDocument;
-
-MyDocument.getInitialProps = async (ctx) => {
-  const originalRenderPage = ctx.renderPage;
-
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App: any) => {
-        const EnhanceApp: FC = (props) => (
-          <App emotionCache={cache} {...props} />
-        );
-        return EnhanceApp;
-      },
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
-      key={style.key}
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
-
-  return {
-    ...initialProps,
-    styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
-  };
-};
